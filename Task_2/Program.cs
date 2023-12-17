0b5cfbdce7d32581b1116885bdf0f397d2b14c28
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:02:03 +0200

    Update Program.cs

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+using Task_2;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        double[] numbers = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+       
+        Console.WriteLine("Сума 5 і 10: " + MathOperations.Add(5, 10));
+
+    
+        Console.WriteLine("Сума чисел масиву: " + MathOperations.Add(numbers));
+
+       
+        Console.WriteLine("Різниця 10 і 5: " + MathOperations.Subtract(10, 5));
+
+    
+        Console.WriteLine("Добуток 5 і 10: " + MathOperations.Multiply(5, 10));
+
+        
+        Console.WriteLine("Частка 10 і 5: " + MathOperations.Divide(10, 5));
+    }
+}
