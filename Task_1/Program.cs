f6cabed75b3e78d1707ba1686490a0fd2db1d5fe
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:01:08 +0200

    Update Program.cs

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Car car = new Car { Speed = 100, Capacity = 5 };
+        Bus bus = new Bus { Speed = 60, Capacity = 30 };
+        Train train = new Train { Speed = 80, Capacity = 200 };
 
-Console.WriteLine("Hello, World!");
+       
+        TransportNetwork transportNetwork = new TransportNetwork();
+        transportNetwork.AddVehicle(car);
+        transportNetwork.AddVehicle(bus);
+        transportNetwork.AddVehicle(train);
+
+      
+        transportNetwork.ControlMovement();
+
+    
+        Route route = new Route { StartPoint = "Київ", EndPoint = "Львів" };
+        Console.WriteLine(route.CalculateOptimalRoute(car));
+        Console.WriteLine(route.CalculateOptimalRoute(bus));
+        Console.WriteLine(route.CalculateOptimalRoute(train));
+
+       
+        Passenger passenger = new Passenger();
+        passenger.Board(car);
+        passenger.Alight(car);
+    }
+}
