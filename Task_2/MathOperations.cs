59d944a7ae683b7d56b0183988c82c220253c954
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:01:41 +0200

    Update MathOperations.cs

--- Task_2/MathOperations.cs
+++ Task_2/MathOperations.cs
@@ -1,6 +1,43 @@
-﻿namespace Task_2;
+namespace Task_2;
 
 public class MathOperations
 {
-    
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+
+    public static double Add(double[] numbers)
+    {
+        double sum = 0;
+        foreach (double num in numbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            throw new DivideByZeroException();
+        }
+    }
 }
